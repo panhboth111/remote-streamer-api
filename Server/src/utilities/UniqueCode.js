dc07a6450d0157984edf2501aa11c8647d728464
CrimsonPiZza <45735628+CrimsonPiZza@users.noreply.github.com>
2020-03-02 22:17:27 +0700

    Resolve  var to let for declearation! #4

--- Server/src/utilities/UniqueCode.js
+++ Server/src/utilities/UniqueCode.js
@@ -1,9 +1,9 @@
 makeUniqueID = (length) => {
     let result           = '';
     const characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    const charactersLength = characters.length;
+    const charactersLengths = characters.length;
     for ( let i = 0; i < length; i++ ) {
-        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+        result += characters.charAt(Math.floor(Math.random() * charactersLengths));
     }
     return result;
 }
