bc4e7a30ed8733f9343f717ce887a17ee50f7300
CrimsonPiZza <45735628+CrimsonPiZza@users.noreply.github.com>
2020-03-11 09:52:37 +0700

    Fix duplication error

--- Server/src/loaders/io.js
+++
@@ -1,9 +0,0 @@
-const app = require('express')()
-const server = app.listen(3002)
-const io = require('socket.io')(server)
-
-module.exports = io
-
-
-
-
--- Server/src/sockets/index.js
+++
@@ -1,7 +0,0 @@
-const Room = require('../models/room')
-const handleChat = require('./handleChats')
-module.exports = (io) => {
-    io.on('connection', async socket => {
-        handleChat(io,socket,Room)
-    })
-}
