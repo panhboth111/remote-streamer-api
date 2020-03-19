01ada9ea07a3b3d2d7ea342b55ece5450f8658b1
Hourn Sovannara <hournsovannara18@kit.edu.kh>
2020-03-19 18:20:48 +0700

    ci: 🎡 Migrate from config.js to .env

--- Chats/src/loaders/database.js
+++ Chats/src/loaders/database.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose')
-const {DBCONNECTION} = require("../../config")
 
 module.exports = async () => {
-    const connection = await mongoose.connect(DBCONNECTION,{useNewUrlParser:true,useUnifiedTopology:true},()=> console.log("database connection established"))
+    const connection = await mongoose.connect(process.env.DBCONNECTION,{useNewUrlParser:true,useUnifiedTopology:true},()=> console.log("database connection established"))
     return connection.connection.db
 }
--- Chats/src/loaders/index.js
+++ Chats/src/loaders/index.js
@@ -1,5 +1,6 @@
 const expressLoader = require('./express')
 const mongoLoader = require('./database')
+require("dotenv").config();
 
 module.exports = async app => {
     await mongoLoader()
